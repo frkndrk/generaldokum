b0085d20202f5c26e73f8e8a7ce92aeb8e9d9703
FURKAN DURAK <furkandurak@FURKAN-MacBook-Air.local>
2024-03-28 23:48:18 +0300

    finished

--- src/components/export/Export.jsx
+++ src/components/export/Export.jsx
@@ -9,18 +9,18 @@ const Export = () => {
             </div>
             <div className="export-map">
                 <img src="../../src/assets/OBJECTS.png" alt="" />
-                <div className="country export-dots-abd"><img src="../../src/assets/abd.svg" alt="" /></div>
-                <div className="country export-dots-en"><img src="../../src/assets/united kingdom.svg" alt="" /></div>
-                <div className="country export-dots-fr"><img src="../../src/assets/france.svg" alt="" /></div>
-                <div className="country export-dots-sp"><img src="../../src/assets/Mask Group 123950.svg" alt="" /></div>
-                <div className="country export-dots-alg"><img src="../../src/assets/algeria.svg" alt="" /></div>
-                <div className="country export-dots-itl"><img src="../../src/assets/italy.svg" alt="" /></div>
-                <div className="country export-dots-slo"><img src="../../src/assets/slovenya.svg" alt="" /></div>
-                <div className="country export-dots-ger"><img src="../../src/assets/germany.svg" alt="" /></div>
-                <div className="country export-dots-den"><img src="../../src/assets/denmark.svg" alt="" /></div>
-                <div className="country export-dots-nth"><img src="../../src/assets/netherland.svg" alt="" /></div>
-                <div className="country export-dots-line"><img src="../../src/assets/Group 312715.svg" alt="" /></div>
-                <div className="country export-federal"><img src="../../src/assets/Group 312714.svg" alt="" /></div>
+                <div className="country export-dots-abd"><img src="../../src/assets/abd.svg" alt="" /><p>Amerika Birleşik Devletleri</p></div>
+                <div className="country export-dots-en"><img src="../../src/assets/united kingdom.svg" alt="" /><p>İngiltere</p></div>
+                <div className="country export-dots-fr"><img src="../../src/assets/france.svg" alt="" /><p>Fransa</p></div>
+                <div className="country export-dots-sp"><img src="../../src/assets/Mask Group 123950.svg" alt="" /><p>İspanya</p></div>
+                <div className="country export-dots-alg"><img src="../../src/assets/algeria.svg" alt="" /><p>Cezayir</p></div>
+                <div className="country export-dots-itl"><img src="../../src/assets/italy.svg" alt="" /><p>İtalya</p></div>
+                <div className="country export-dots-slo"><img src="../../src/assets/slovenya.svg" alt="" /><p>Slovenya</p></div>
+                <div className="country export-dots-ger"><img src="../../src/assets/germany.svg" alt="" /><p>Almanya</p></div>
+                <div className="country export-dots-den"><img src="../../src/assets/denmark.svg" alt="" /><p>Danimarka</p></div>
+                <div className="country export-dots-nth"><img src="../../src/assets/netherland.svg" alt="" /><p>Hollanda</p></div>
+                <div className="country export-dots-line"><img src="../../src/assets/Group 312715.svg" alt="" /><p></p></div>
+                <div className="country export-federal"><img src="../../src/assets/Group 312714.svg" alt="" /><p>Federal Döküm</p></div>
             </div>
 
         </div>
