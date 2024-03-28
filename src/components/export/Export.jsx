50f9df3c2c17966a85172be37a454d16716db7d2
FURKAN DURAK <furkandurak@FURKAN-MacBook-Air.local>
2024-03-28 14:40:37 +0300

    last upload

--- src/components/export/Export.jsx
+++ src/components/export/Export.jsx
@@ -28,33 +28,3 @@ const Export = () => {
 }
 
 export default Export
-
-{/*         <div className="export-map">
-                <img src="../../src/assets/OBJECTS.png" alt="" />
-                <div className="country export-dots-abd"><img src="../../src/assets/abd.svg" alt="" /></div>
-                <div className="country export-dots-en"><img src="../../src/assets/united kingdom.svg" alt="" /></div>
-                <div className="country export-dots-fr"><img src="../../src/assets/france.svg" alt="" /></div>
-                <div className="country export-dots-sp"><img src="../../src/assets/Mask Group 123950.svg" alt="" /></div>
-                <div className="country export-dots-alg"><img src="../../src/assets/algeria.svg" alt="" /></div>
-                <div className="country export-dots-itl"><img src="../../src/assets/italy.svg" alt="" /></div>
-                <div className="country export-dots-slo"><img src="../../src/assets/slovenya.svg" alt="" /></div>
-                <div className="country export-dots-ger"><img src="../../src/assets/germany.svg" alt="" /></div>
-                <div className="country export-dots-den"><img src="../../src/assets/denmark.svg" alt="" /></div>
-                <div className="country export-dots-nth"><img src="../../src/assets/netherland.svg" alt="" /></div>
-                <div className="country export-dots-line"><img src="../../src/assets/Group 312715.svg" alt="" /></div>
-                <div className="country export-federal"><img src="../../src/assets/Group 312714.svg" alt="" /></div>
-            </div> */}
-
-{/* <img src="../../src/assets/abd.svg" alt="" />
-                <img src="../../src/assets/united kingdom.svg" alt="" />
-                <img src="../../src/assets/france.svg" alt="" />
-                <img src="../../src/assets/Mask Group 123950.svg" alt="" />
-                <img src="../../src/assets/algeria.svg" alt="" />
-                <img src="../../src/assets/italy.svg" alt="" />
-                <img src="../../src/assets/slovenya.svg" alt="" />
-                <img src="../../src/assets/germany.svg" alt="" />
-                <img src="../../src/assets/denmark.svg" alt="" />
-                <img src="../../src/assets/netherland.svg" alt="" />
-                <img src="../../src/assets/Group 312715.svg" alt="" />
-                <img src="../../src/assets/Group 312714.svg" alt="" />
- */}
